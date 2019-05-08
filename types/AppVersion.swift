a27c8569c642087ad77ca814b3388c2e77276f2d
Gonzo Fialho <4302061+gonzula@users.noreply.github.com>
2019-05-08 10:23:05 -0300

    Create AppVersion.swift

---
+++ types/AppVersion.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+typealias AppVersion = ValidatedString<AppVersionValidator>
+
+enum AppVersionValidator: StringValidator, StringComparator {
+    static func validate(_ string: String) -> String? {
+        let string = string.trimmingCharacters(in: .whitespacesAndNewlines)
+        var charset = CharacterSet.decimalDigits
+        charset.insert(".")
+        let filtered = string.components(separatedBy: charset.inverted).joined()
+        guard filtered == string else {return nil}
+
+        return filtered
+    }
+
+    static func areInIncreasingOrder(_ lhs: String, _ rhs: String) -> Bool {
+        let leftComponents = lhs.components(separatedBy: ".")
+        let rightComponents = rhs.components(separatedBy: ".")
+
+        for (leftSubversion, rightSubversion) in zip(leftComponents, rightComponents) {
+            if leftSubversion != rightSubversion {
+                return Int(leftSubversion)! < Int(rightSubversion)!
+            }
+        }
+
+        return leftComponents.count < rightComponents.count
+    }
+}
